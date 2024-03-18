7a827159b9f773682e3115875ddd0e5f5ed66d1a
Lassi Lahtonen <AE8656@student.jamk.fi>
2024-03-18 17:06:45 +0200

    added two new components

---
+++ src/lib/Heading.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let heading;
+</script>
+
+<div class="box">
+  <h1><b>{heading}</b></h1>
+</div>
+
+<style>
+  h1 {
+    font-size: 5em;
+    color: blueviolet;
+    font-family: 'Times New Roman', Times, serif;
+  }
+
+  .box {
+    background-color: black;
+    padding-right: 0.6em;
+    padding-left: 0.6em;
+  }
+</style>
