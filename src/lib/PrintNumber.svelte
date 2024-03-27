51a8698433b61490fff8bd2b0f4dd3459131e4fb
Lassi Lahtonen <AE8656@student.jamk.fi>
2024-03-27 10:15:22 +0200

    added a component for printing the drawn numbers

---
+++ src/lib/PrintNumber.svelte
@@ -0,0 +1,29 @@
+<script>
+  import printNumber from '../numberPrintStore';
+  import { onDestroy } from 'svelte';
+
+  let numbers;
+
+  const unsub = printNumber.subscribe(
+    (randomNumbers) => (numbers = randomNumbers)
+  );
+
+  onDestroy(() => {
+    if (unsub) {
+      unsub();
+    }
+  });
+</script>
+
+{#each numbers as number}
+  <div class="ball">
+    {number}
+  </div>
+{/each}
+
+<style>
+  .ball {
+    border: 1px solid white;
+    border-radius: 50px;
+  }
+</style>
