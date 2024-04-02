7f9c83550fdd22f56aa0907452030dc4b9e069a5
Lassi Lahtonen <AE8656@student.jamk.fi>
2024-04-02 14:02:23 +0300

    added a new component for transfering money

---
+++ src/lib/TransferMoney.svelte
@@ -0,0 +1,36 @@
+<script>
+</script>
+
+<div class="backdrop" />
+
+<div class="modal">
+  <header>Transfer money</header>
+</div>
+
+<style>
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1000vh;
+    background: rgba(57, 57, 57, 0.7);
+    z-index: 10;
+  }
+
+  .modal {
+    position: fixed;
+    top: 200px;
+    left: 305px;
+    width: 60%;
+    max-height: 80vh;
+    background: white;
+    border-radius: 50px;
+    z-index: 100;
+  }
+
+  header {
+    font-size: 3rem;
+    color: black;
+  }
+</style>
